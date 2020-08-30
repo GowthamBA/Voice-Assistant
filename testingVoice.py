8f3364c68f81be57fa614b28d3563b05b61d7f47
GowthamBA <48147531+GowthamBA@users.noreply.github.com>
2020-08-30 16:52:09 +0530

    Create testingVoice.py
    Recognition testing series

---
+++ testingVoice.py
@@ -0,0 +1,9 @@
+recording = sr.Recognizer()
+with sr.Microphone() as source: 
+    recording.adjust_for_ambient_noise(source)
+    print("Please Say something:")
+    audio = recording.listen(source)
+try:
+    print("You said: \n" + recording.recognize_google(audio))
+except Exception as e:
+    print(e)
